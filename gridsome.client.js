ba68007ad37bdd87b5a944ea6f669fdb9d276fab
securized <root@securized.dev>
2021-01-18 15:21:27 +1100

    fix - send only production hits

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,4 +1,9 @@
 export default function (Vue, options, { head }) {
+  if (process.env.NODE_ENV !== 'production') {
+    // Only send hits on production builds
+    return;
+  }
+
   head.script.push({
     src: 'https://static.cloudflareinsights.com/beacon.min.js',
     defer: true,
